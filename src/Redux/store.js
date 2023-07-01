d03e4cbae407965a6092a2d815aabf69070a6c1c
MosDevx <mosesk.wamae@gmail.com>
2023-07-01 21:36:22 +0300

    linters

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -16,3 +16,5 @@ export const store = configureStore({
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
+
+export default store;
